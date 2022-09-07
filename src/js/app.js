ad27c6f767ab6cd94bd70469006ae464184e0afc
Qbik222 <97127218+Qbik222@users.noreply.github.com>
2022-09-07 16:48:28 +0300

    create section advantages and create js function addAdventegesCard

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,126 @@
 import * as flsFunctions from "./modules/functions.js";
  flsFunctions.isWebp();
+
+window.addEventListener("DOMContentLoaded", () =>{
+
+class AdventegesCard {
+    constructor(src, alt, title, descr, parrentSelector ){
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.parrent = document.querySelector(parrentSelector);
+
+    }
+
+    render(){
+        const element = document.createElement("div");
+        element.classList.add("advantages__item");
+
+        element.innerHTML = `
+            <div class="adventeges__img">
+                <img src="${this.src}" alt="${this.alt}">
+            </div>
+            <p>
+                <span>"${this.title}"</span> <br>
+              "${this.descr}"
+            </p>
+            `;
+        this.parrent.append(element);
+
+    }
+}
+
+
+function addAdventegesCard (src, alt, title, descr, parentSelector){
+    new AdventegesCard(
+        src,
+        alt,
+        title,
+        descr,
+        parentSelector,
+    ).render();
+}
+
+addAdventegesCard( "./img/Wallet.png",
+"wallet",
+"Никаких наличных, оплата происходит автоматически",
+`Стоимость поездки оплачивается пассажиром привязанной банковской карты, 
+т.е. нет никаких рисков связанныхс перевозкой наличности как 
+для пассажира, так и для водителя, Вам не нужно искать мелкие деньги 
+для размена`,
+".advantages__content");
+addAdventegesCard("./img/money.png",
+"money",
+"Высокий доход с гарантированными выплатами",
+`Мы выплачиваем заработную плату на личную 
+банковскую карту водителя! Вам не нужно 
+оформлять ООО, ИП, платить налоги, подавать 
+декларацию, оплачивать бухгалтерские услуги
+ и ведение банковского счета.`,
+ ".advantages__content",
+);
+addAdventegesCard("./img/money.png",
+"money",
+"Высокий доход с гарантированными выплатами",
+`Мы выплачиваем заработную плату на личную 
+банковскую карту водителя! Вам не нужно 
+оформлять ООО, ИП, платить налоги, подавать 
+декларацию, оплачивать бухгалтерские услуги
+ и ведение банковского счета.`,
+ ".advantages__content",
+);
+addAdventegesCard("./img/money.png",
+"money",
+"Высокий доход с гарантированными выплатами",
+`Мы выплачиваем заработную плату на личную 
+банковскую карту водителя! Вам не нужно 
+оформлять ООО, ИП, платить налоги, подавать 
+декларацию, оплачивать бухгалтерские услуги
+ и ведение банковского счета.`,
+ ".advantages__content",
+);
+addAdventegesCard("./img/money.png",
+"money",
+"Высокий доход с гарантированными выплатами",
+`Мы выплачиваем заработную плату на личную 
+банковскую карту водителя! Вам не нужно 
+оформлять ООО, ИП, платить налоги, подавать 
+декларацию, оплачивать бухгалтерские услуги
+ и ведение банковского счета.`,
+ ".advantages__content",
+);
+addAdventegesCard("./img/money.png",
+"money",
+"Высокий доход с гарантированными выплатами",
+`Мы выплачиваем заработную плату на личную 
+банковскую карту водителя! Вам не нужно 
+оформлять ООО, ИП, платить налоги, подавать 
+декларацию, оплачивать бухгалтерские услуги
+ и ведение банковского счета.`,
+ ".advantages__content",
+);
+addAdventegesCard("./img/money.png",
+"money",
+"Высокий доход с гарантированными выплатами",
+`Мы выплачиваем заработную плату на личную 
+банковскую карту водителя! Вам не нужно 
+оформлять ООО, ИП, платить налоги, подавать 
+декларацию, оплачивать бухгалтерские услуги
+ и ведение банковского счета.`,
+ ".advantages__content",
+);
+addAdventegesCard("./img/money.png",
+"money",
+"Высокий доход с гарантированными выплатами",
+`Мы выплачиваем заработную плату на личную 
+банковскую карту водителя! Вам не нужно 
+оформлять ООО, ИП, платить налоги, подавать 
+декларацию, оплачивать бухгалтерские услуги
+ и ведение банковского счета.`,
+ ".advantages__content",
+);
+
+
+
+});
